207a19e0f8e14bc602f723fcf4a205febd1d116e
rangaraju <rangaraju29139@gmail.com>
2017-10-04 13:34:52 +0530

    i am completely done with this new feature

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ console.log('i\'m something');
 }
 function aBignewfunction(){
 console.log("this is a big new function");
+console.log("i\'m comoletely done with this new feature");
 }
 aBignewfunction();
 console.log('hello world');
