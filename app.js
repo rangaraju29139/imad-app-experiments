8ea7f0a9d03b25bc1d1329563ccd65c0d052bdd1
rangaraju <rangaraju29139@gmail.com>
2017-10-04 13:26:26 +0530

    i\'m half way done

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 function something(){
 console.log('i\'m something');
 }
+function aBignewfunction(){
+console.log("this is a big new function");
+}
+aBignewfunction();
 console.log('hello world');
 something();
